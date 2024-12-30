895417c00a828e8cb238793d691f7495d595ff4b
datnguyenbhx <duc.dat@beehexa.com>
2024-12-30 08:20:46 +0700

    backup

--- src/SqlAccountRestAPI/Core/SqlAccountFactory.cs
+++ src/SqlAccountRestAPI/Core/SqlAccountFactory.cs
@@ -5,37 +5,37 @@ namespace SqlAccountRestAPI.Core;
 
 public class SqlAccountingFactory : IDisposable
 {
-    [DllImport("kernel32.dll", SetLastError = true)]
-    public static extern uint WTSGetActiveConsoleSessionId();
+//     [DllImport("kernel32.dll", SetLastError = true)]
+//     public static extern uint WTSGetActiveConsoleSessionId();
     
-    [DllImport("wtsapi32.dll", SetLastError = true)]
-    public static extern bool WTSQuerySessionInformation(IntPtr hServer, uint sessionId, WTS_INFO_CLASS wtsInfoClass, out IntPtr ppBuffer, out uint pBytesReturned);
+//     [DllImport("wtsapi32.dll", SetLastError = true)]
+//     public static extern bool WTSQuerySessionInformation(IntPtr hServer, uint sessionId, WTS_INFO_CLASS wtsInfoClass, out IntPtr ppBuffer, out uint pBytesReturned);
 
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern bool SetThreadDesktop(uint hDesktop);
+//     [DllImport("user32.dll", SetLastError = true)]
+//     public static extern bool SetThreadDesktop(uint hDesktop);
 
-    public enum WTS_INFO_CLASS
-{
-    WTSUserName = 5,
-    WTSDomainName = 7,
-    WTSConnectState = 8,
-    WTSClientName = 10,
-    WTSClientAddress = 14,
-    WTSIdleTime = 15,
-    WTSLogonTime = 16,
-    WTSIncomingBytes = 17,
-    WTSOutgoingBytes = 18,
-    WTSIncomingFrames = 19,
-    WTSOutgoingFrames = 20,
-    WTSClientProtocolType = 22,
-    WTSClientDirectory = 23,
-    WTSClientBuildNumber = 24,
-    WTSClientNameLength = 25,
-    WTSClientAddressLength = 26,
-    WTSClientDirectoryLength = 27,
-    WTSClientBuildNumberLength = 28,
-    WTSSessionId = 29,
-}
+//     public enum WTS_INFO_CLASS
+// {
+//     WTSUserName = 5,
+//     WTSDomainName = 7,
+//     WTSConnectState = 8,
+//     WTSClientName = 10,
+//     WTSClientAddress = 14,
+//     WTSIdleTime = 15,
+//     WTSLogonTime = 16,
+//     WTSIncomingBytes = 17,
+//     WTSOutgoingBytes = 18,
+//     WTSIncomingFrames = 19,
+//     WTSOutgoingFrames = 20,
+//     WTSClientProtocolType = 22,
+//     WTSClientDirectory = 23,
+//     WTSClientBuildNumber = 24,
+//     WTSClientNameLength = 25,
+//     WTSClientAddressLength = 26,
+//     WTSClientDirectoryLength = 27,
+//     WTSClientBuildNumberLength = 28,
+//     WTSSessionId = 29,
+// }
 
     private dynamic? _app = null;
     public dynamic GetInstance()
@@ -61,15 +61,15 @@ public class SqlAccountingFactory : IDisposable
                 throw new Exception("Cannot load SQLAcc.BizApp Assembly");
             }
             
-            uint sessionId = WTSGetActiveConsoleSessionId();
+            // uint sessionId = WTSGetActiveConsoleSessionId();
 
-            IntPtr buffer;
-            uint bytesReturned;
-            WTSQuerySessionInformation(IntPtr.Zero, sessionId, WTS_INFO_CLASS.WTSSessionId, out buffer, out bytesReturned);
+            // IntPtr buffer;
+            // uint bytesReturned;
+            // WTSQuerySessionInformation(IntPtr.Zero, sessionId, WTS_INFO_CLASS.WTSSessionId, out buffer, out bytesReturned);
 
-            IntPtr userSessionId = Marshal.ReadIntPtr(buffer);
+            // IntPtr userSessionId = Marshal.ReadIntPtr(buffer);
 
-            SetThreadDesktop((uint)Marshal.ReadInt32(userSessionId));
+            // SetThreadDesktop((uint)Marshal.ReadInt32(userSessionId));
 
             _app = Activator.CreateInstance(lBizType);
 
