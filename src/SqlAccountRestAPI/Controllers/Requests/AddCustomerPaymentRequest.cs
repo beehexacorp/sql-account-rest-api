5e976989d4e1018585e69c569eeb1227e9672d67
datnguyenbhx <duc.dat@beehexa.com>
2025-02-03 06:52:10 +0700

    add comment for old release

--- src/SqlAccountRestAPI/Controllers/Requests/AddCustomerPaymentRequest.cs
+++ src/SqlAccountRestAPI/Controllers/Requests/AddCustomerPaymentRequest.cs
@@ -5,4 +5,5 @@ public class AddCustomerPaymentRequest
     public string DocumentNo { get; set; } = null!;
     public string PaymentMethod { get; set; } = null!;
     public string Project { get; set; } = null!;
+    public string Code { get; set; } = null!; // Release-0.0.42 and older
 }
